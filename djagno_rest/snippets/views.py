022aa80b8e1c26fcf88309ad6faf994f4d5c6967
adithyan-777 <adithyan92014@gmail.com>
2024-09-10 20:00:23 +0530

    reverse and hyperlinked

--- djagno_rest/snippets/views.py
+++ djagno_rest/snippets/views.py
@@ -10,10 +10,36 @@ from rest_framework.views import APIView
 from rest_framework.parsers import JSONParser
 from rest_framework import generics
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
+from snippets.serializers import SnippetSerializer, UserSerializer
+from django.contrib.auth.models import User
+from rest_framework import permissions
+from snippets.permissions import IsOwnerOrReadOnly
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from rest_framework import renderers
 
 
-# @api_view(["GET", "POST"])
+@api_view(["GET"])
+def api_root(request, format=None):
+    return Response(
+        {
+            "users": reverse("user-list", request=request, format=format),
+            "snippets": reverse("snippet-list", request=request, format=format),
+        }
+    )
+
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
+
+
 class SnippetList(generics.ListCreateAPIView):
     """
     List all code snippets, or create a new snippet.
@@ -21,22 +47,12 @@ class SnippetList(generics.ListCreateAPIView):
 
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-    # def get(self, request, format=None):
-    #     snippets = Snippet.objects.all()
-    #     serializer = SnippetSerializer(snippets, many=True)
-    #     return Response(serializer.data)
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
-    # def post(self, request, format=None):
-    #     data = JSONParser().parse(request)
-    #     serializer = SnippetSerializer(data=request.data)
-    #     if serializer.is_valid():
-    #         serializer.save()
-    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
-    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
-# @api_view(["GET", "POST", "DELETE"])
-# def snippet_detail(request, pk, format=None):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     Retrieve, update or delete a code snippet.
@@ -44,28 +60,14 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
 
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-    # def get_object(self, pk):
-    #     try:
-    #         snippet = Snippet.objects.get(pk=pk)
-    #     except Snippet.DoesNotExist:
-    #         raise Http404
-    #         # return Response(status=status.HTTP_404_NOT_FOUND)
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
-    # def get(self, request, pk, format=None):
-    #     snippet = self.get_object(pk=pk)
-    #     serializer = SnippetSerializer(snippet)
-    #     return Response(serializer.data)
 
-    # def put(self, request, pk, format=None):
-    #     snippet = self.get_object(pk=pk)
-    #     serializer = SnippetSerializer(snippet, data=request.data)
-    #     if serializer.is_valid():
-    #         serializer.save()
-    #         return Response(serializer.data)
-    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
-    # def delelte(self, request, pk, format=None):
-    #     snippet = self.get_object(pk)
-    #     if snippet:
-    #         snippet.delete()
-    #     return Response(status=status.HTTP_204_NO_CONTENT)
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
